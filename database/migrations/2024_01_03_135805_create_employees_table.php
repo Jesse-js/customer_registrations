d9bd031941137a064ae828c75331d940bd99ac87
Jesse-js <jesse-jjs@hotmail.com>
2024-01-03 11:15:16 -0300

    feat: create employees table

---
+++ database/migrations/2024_01_03_135805_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->char('cpf', 11);
+            $table->date('hiring_date');
+            $table->date('resignation_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
